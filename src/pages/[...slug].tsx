50d5b6fa466c11928bd0c4db50d999c1675aec9b
Hassen <hassen.hichri@outlook.com>
2024-11-02 16:29:20 +0100

    rollback nextjs version

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -15,13 +15,8 @@ export const getStaticProps = (async () => {
     return {
         notFound: true,
     }
-    const res = await fetch('https://api.github.com/repos/vercel/next.js')
-    const repo = await res.json()
-    return { revalidate: 1, props: { repo } }
 })
 
-export default function Page({
-                                 repo,
-                             }: InferGetStaticPropsType<typeof getStaticProps>) {
-    return repo.stargazers_count
+export default function Page() {
+    return <p>hello world!</p>
 }
